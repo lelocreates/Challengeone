88ae4838a1b41a04938bce58e9c2fda88172f1d8
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-17 11:57:19 +0200

    Updated code for black jack game

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ let sum = firstCard + secondCard
 
 //console.log(sum)
 
-if (sum < 21){ 
+if (sum <= 20){ 
     console.log("Do you want to draw a new card 🙂")
 } else if (sum === 21 ) {
     console.log("Wohoo! You've got Blackjack!🥳")
-} else if (sum > 21) {
+} else {
     console.log("You're out of the game!😭")
 }
 
