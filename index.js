38fc7e0c35989db984916e7a2125c151e4524bd8
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-17 11:39:16 +0200

    Add code to check if user is older than 21

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ if (sum < 21){
 } else if (sum > 21) {
     console.log("You're out of the game!😭")
 }
+
+// Check if the person is old enough to enter the nightclub (21)
+// Log a suitable message to the console in both cases
+
+let age = 22 
+
+if (age < 21) {
+    console.log("You can not enter the club")
+} else {
+    console.log("Welcome!")
+}
