b796c4dda335a51c6c647e51575fe44bcbf1720e
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-17 11:47:54 +0200

    Create sidequests file with age checker code
    Other code practise doen during the black jack game journey on Scrimba

---
+++ sidequests.js
@@ -0,0 +1,11 @@
+// Check if the person is elegible for a birthday card from the King! (100) 
+
+let age = 100
+
+if(age<100) {
+    console.log("Not elegible")
+} else if ( age === 100) {
+    console.log("Here is your birthday card from the King!")
+}else {
+    console.log("Not elegible, you have already gotten one")
+}
