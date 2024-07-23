f0c8fe1a1762e6fd139a48af1af3f4d08a4122c7
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-24 01:40:49 +0200

    Update JS file

--- index.js
+++ index.js
@@ -7,24 +7,37 @@ let firstCard = 6
 let secondCard = 9 
 
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 //console.log(sum)
 
 if (sum <= 20){ 
-    console.log("Do you want to draw a new card 🙂")
+    message= "Do you want to draw a new card 🙂"
 } else if (sum === 21 ) {
-    console.log("Wohoo! You've got Blackjack!🥳")
+    message= "Wohoo! You've got Blackjack!🥳"
+    hasBlackJack = true
 } else {
-    console.log("You're out of the game!😭")
+    message= "You're out of the game!😭"
+    isAlive = false
 }
 
 // Check if the person is old enough to enter the nightclub (21)
 // Log a suitable message to the console in both cases
 
-let age = 22 
+//let age = 22 
 
-if (age <= 21) {
-    console.log("You can not enter the club")
-} else {
-    console.log("Welcome!")
-}
+//if (age <= 21) {
+ //   console.log("You can not enter the club")
+//} else {
+ //   console.log("Welcome!")
+//}
+
+
+// CASH OUT!
+console.log(hasBlackJack)
+console.log(isAlive)
+console.log(message)
+
+
